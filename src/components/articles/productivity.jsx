a12ec3a258df8f5d278efa7709b8767a155dd76d
Viktor Holta <viktor.holta@outlook.com>
2022-03-21 15:37:50 +0100

    Add form for logging actions & list for displaying them

--- src/components/articles/productivity.jsx
+++ src/components/articles/productivity.jsx
@@ -1,3 +1,8 @@
+import { mutate } from "swr"
+import { del, post } from "../../utils/request"
+
+import { Button } from "../input/buttons"
+
 export function ActionBarChart({ data }) {
   let bars = []
   for (let i = 0; i < data.day_count; i++) {
@@ -25,5 +30,64 @@ export function ActionBarChart({ data }) {
       <h2>Minutes of Productivity per Day</h2>
       <div className="bar-chart">{bars}</div>
     </article>
+  )
+}
+
+export function ActionEntry({ data }) {
+  const durationParsed = parseFloat(data.duration)
+  const durationSec = Math.floor(durationParsed % 60)
+  const durationMin = Math.floor(durationParsed / 60)
+  const durationString = `${String(durationMin).padStart(2, "0")}:${String(durationSec).padStart(2, "0")}`
+
+  const deleteAction = () => {
+    del(`/productivity/entries/${data.id}/`, true).then(res => {
+      mutate(["/productivity/entries/", true])
+      mutate("/productivity/statistics/")
+    })
+  }
+
+  return (
+    <div className="action-entry row">
+      <h4>
+        {data.start_obj.category}
+        <span> - </span>
+        {data.start_obj.description}
+        <span> - </span>
+        {durationString}
+      </h4>
+
+      <div className="spacer" />
+      <Button onClick={deleteAction} label="Delete" />
+    </div>
+  )
+}
+
+export function ActionStart({ data }) {
+  const endAction = () => {
+    post("/productivity/entries/", { start: data.id }, true).then(res => {
+      mutate(["/productivity/starts/", true])
+      mutate(["/productivity/entries/", true])
+      mutate("/productivity/statistics/")
+    })
+  }
+
+  const deleteAction = () => {
+    del(`/productivity/starts/${data.id}/`, true).then(res => {
+      mutate(["/productivity/starts/", true])
+    })
+  }
+
+  return (
+    <div className="action-start row">
+      <h4>
+        {data.category}
+        <span> - </span>
+        {data.description}
+      </h4>
+
+      <div className="spacer" />
+      <Button onClick={endAction} label="End" />
+      <Button onClick={deleteAction} label="Delete" />
+    </div>
   )
 }
--- src/pages/productivity.jsx
+++ src/pages/productivity.jsx
@@ -1,13 +1,53 @@
+import { mutate } from "swr"
 import useSWR from "swr"
+import { post } from "../utils/request"
 
-import { ActionBarChart } from "../components/articles/productivity"
+import { ActionBarChart, ActionEntry, ActionStart } from "../components/articles/productivity"
+import Form from "../components/articles/form"
+import { SingleLineField, SelectMenu } from "../components/input/fields"
+import { SubmitButton } from "../components/input/buttons"
 
 export default function ProductivityPage({ me }) {
   const { data: statistics } = useSWR("/productivity/statistics/")
+  const { data: categories } = useSWR("/productivity/categories/")
+  const { data: entries } = useSWR(["/productivity/entries/", true])
+  const { data: starts } = useSWR(["/productivity/starts/", true])
+
+  const categoryList = []
+  if (categories)
+    categories.map(category => categoryList.push(category.name))
+
+  const logAction = (e) => {
+    const logData = {
+      category: e.target.category.value,
+      description: e.target.description.value,
+    }
+  
+    post("/productivity/starts/", logData, true).then(res => {
+      e.target.reset()
+      mutate(["/productivity/starts/", true])
+    })
+  }
 
   return (
     <main>
+      <Form title="Log an Action" onSubmit={logAction}>
+        <SingleLineField id="desc" name="description" 
+          label="Description" placeholder="Enter description here..." />
+        <SelectMenu id="category" name="category" label="Category" options={categoryList} 
+          values={categoryList} defaultOption="-- Please select a category --" />
+        <SubmitButton label="Log" />
+      </Form>
+
       {statistics && <ActionBarChart data={statistics} />}
+
+      {me && me.is_superuser && entries && starts && (
+        <article className="actions">
+          <h2>Actions of Today</h2>
+          {starts.map((start, i) => <ActionStart key={i} data={start} />)}
+          {entries.map((entry, i) => <ActionEntry key={i} data={entry} />)}
+        </article>
+      )}
     </main>
   )
 }
