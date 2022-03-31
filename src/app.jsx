fbbf3e251fc582897b9df464357c4516bc5a241c
Viktor Holta <viktor.holta@outlook.com>
2022-03-31 15:53:42 +0200

    Remove almost everything

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,8 @@
 import { Route, Switch } from "react-router-dom"
 import useSWR, { mutate } from "swr"
 
-import "./app.scss"
-import "./misc.scss"
-
-import { LinkButton, Button } from "./components/input/buttons"
-
-import HomePage from "./pages/home"
-import SoftwarePage from "./pages/software"
-import ContactPage from "./pages/contact"
-import ProductivityPage from "./pages/productivity"
-import LoginPage from "./pages/login"
-import RegisterPage from "./pages/register"
+import "./styles/app.scss"
+import "./styles/misc.scss"
 
 export default function App() {
   const { data: me } = useSWR(["/accounts/me/", true])
@@ -20,47 +11,14 @@ export default function App() {
     window.localStorage.removeItem("token")
     mutate(["/accounts/me/", true], undefined)
   }
-  
 
   return (
     <div className="app">
-      <aside>
-        <h1>Parslie</h1>
-        <nav>
-          <LinkButton to="/" label="Home" />
-          <LinkButton to="/software" label="Software" />
-          <LinkButton to="/contact" label="Contact" />
-          <LinkButton to="/productivity" label="Productivity" />
-
-          {me ? <Button label={`Log Out as ${me.username}`} onClick={logOut} /> : (
-            <>
-              <LinkButton to="/login" label="Log In" />
-              <LinkButton to="/register" label="Register" />
-            </>
-          )}
-        </nav>
-      </aside>
+      <header>
+      </header>
 
       <main>
         <Switch>
-          <Route exact path="/">
-              <HomePage me={me} />
-          </Route>
-          <Route path="/software">
-              <SoftwarePage me={me} />
-          </Route>
-          <Route path="/contact">
-              <ContactPage me={me} />
-          </Route>
-          <Route path="/productivity">
-              <ProductivityPage me={me} />
-          </Route>
-          <Route path="/login">
-              <LoginPage me={me} />
-          </Route>
-          <Route path="/register">
-              <RegisterPage me={me} />
-          </Route>
         </Switch>
       </main>
     </div>
