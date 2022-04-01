65ac1576b8cc1516b37c2699b16cb6065c977a33
Viktor Holta <viktor.holta@outlook.com>
2022-04-01 13:12:29 +0200

    Implement post creation and error-handling for software

--- src/pages/software.jsx
+++ src/pages/software.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 
+import useSWR, { mutate } from "swr"
+import { post } from "../utils/request"
+
 import Form from "../components/form"
 import { Post } from "../components/software"
 import { MultiLineText, SingleLineText } from "../components/fields"
@@ -15,11 +18,33 @@ export default function SoftwarePage({ me }) {
   }
 
   // TODO: remove debug above
-  const [ titleError, setTitleError ] = useState("Test error")
-  const [ contentError, setContentError ] = useState("Test content error")
+  const { data: posts } = useSWR("/posts/software/")
+
+  const [ titleError, setTitleError ] = useState()
+  const [ contentError, setContentError ] = useState()
+  const [ repositoryError, setRepositoryError ] = useState()
+  const [ generalError, setGeneralError ] = useState()
 
   const createPost = (e) => {
-    
+    const postData = {
+      title: e.target.title.value,
+      content: e.target.content.value,
+      repository: `${e.target.repoOwner.value}/${e.target.repoName.value}`,
+    }
+
+    post("/posts/software/", postData, true).then(res => {
+      e.target.reset()
+      setTitleError("")
+      setContentError("")
+      setRepositoryError("")
+      setGeneralError("")
+      mutate("/posts/software/")
+    }).catch(({ response: res }) => {
+      setTitleError(res.data.title)
+      setContentError(res.data.content)
+      setRepositoryError(res.data.repository)
+      setGeneralError(`${res.status} ${res.statusText}`)
+    })
   }
 
   return (
@@ -27,21 +52,13 @@ export default function SoftwarePage({ me }) {
       <Form onSubmit={createPost} title="Create a Software Post">
         <SingleLineText name="title" placeholder="Enter post title here..." error={titleError} />
         <MultiLineText name="content" placeholder="Enter post content here..." error={contentError} />
-        <SubmitButton label="Post" />
+        <SingleLineText name="repoOwner" placeholder="Enter repository owner here..." />
+        <SingleLineText name="repoName" placeholder="Enter repository name here..."
+           error={repositoryError} />
+        <SubmitButton label="Post" error={generalError} />
       </Form>
 
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
-      <Post data={mockPostData} me={me} />
+      {posts && posts.map((postData, i) => <Post data={postData} key={i} />)}
     </main>
   )
 }
