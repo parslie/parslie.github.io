6594bf9898efdc0e095c441b5ac6df857b4f9c84
Viktor Holta <viktor.holta@outlook.com>
2022-05-08 19:01:01 +0200

    Style header, nav, and main elements of app

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,19 @@
+import { useHistory } from "react-router-dom";
 import "../styles/app.scss";
+
+function NavButton({ to, label }) {
+  const history = useHistory();
+  return <input type="button" value={label} onClick={() => history.push(to)} />;
+}
 
 function AppHeader() {
   return (
     <header>
       <h1>Parslie</h1>
+      <nav>
+        <NavButton to="/" label="Home" />
+        <NavButton to="/actions" label="Actions" />
+      </nav>
     </header>
   );
 }
