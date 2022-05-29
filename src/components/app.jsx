a412cdceb69712baad9dc8f5dcbb1a3701d2f250
Viktor Holta <viktor.holta@outlook.com>
2022-05-29 22:46:31 +0200

    Prepare log in, log out, and register prompts

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,34 +1,92 @@
+import { useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import { useHistory } from "react-router-dom";
-import useSWR from "swr";
+
+import useSWR, { mutate } from "swr";
+import { post } from "../utils/request";
 
 import "../styles/app.scss";
 import { FormDemoPage, GraphDemoPage } from "../pages/demo";
 import ActionPage from "../pages/action";
 import AboutPage from "../pages/about";
+import Prompt from "./prompt";
+import Form from "./form";
+import { ButtonField, SubmitField, TextField } from "./input";
 
 function NavButton({ to, label }) {
   const history = useHistory();
   return <button onClick={() => history.push(to)}>{label}</button>;
 }
 
-function AppHeader() {
+function AppHeader({ me }) {
+  const [ showLogOutPrompt, setShowLogOutPrompt ] = useState(false);
+  const [ showLogInPrompt, setShowLogInPrompt ] = useState(false);
+  const [ showRegisterPrompt, setShowRegisterPrompt ] = useState(false);
+
+  const logOut = () => {
+    
+  };
+
+  const logIn = (e) => {
+    
+  };
+
+  const register = (e) => {
+    
+  };
+
   return (
     <header>
       <h1>Parslie</h1>
       <nav>
         <NavButton to="/" label="About" />
+        {me && <button onClick={() => setShowLogOutPrompt(true)}>Log Out</button>}
+        {!me && <button onClick={() => setShowLogInPrompt(true)}>Log In</button>}
+        {!me && <button onClick={() => setShowRegisterPrompt(true)}>Register</button>}
+        
+        {showLogOutPrompt && (
+          <Prompt title="Are you sure you want to log out?" onCancel={() => setShowLogOutPrompt(false)}>
+            <Form>
+              <ButtonField label="Yes" onClick={logOut} />
+              <ButtonField label="No" onClick={() => setShowLogOutPrompt(false)} />
+            </Form>
+          </Prompt>
+        )}
+        
+        {showLogInPrompt && (
+          <Prompt title="Log In" onCancel={() => setShowLogInPrompt(false)}>
+            <Form onSubmit={logIn}>
+              <TextField name="email" placeholder="Enter e-mail here..." type="email" />
+              <TextField name="password" placeholder="Enter password here..." type="password" />
+              <SubmitField label="Log In" />
+            </Form>
+          </Prompt>
+        )}
+        
+        {showRegisterPrompt && (
+          <Prompt title="Register" onCancel={() => setShowRegisterPrompt(false)}>
+            <Form onSubmit={register}>
+              <TextField name="email" placeholder="Enter e-mail here..." type="email" />
+              <TextField name="username" placeholder="Enter username here..." />
+              <TextField name="password" placeholder="Enter password here..." type="password" />
+              <TextField name="confirmation" placeholder="Confirm password here..." type="password" />
+              <SubmitField label="Register " />
+            </Form>
+          </Prompt>
+        )}
+
+        {/*
         <h3>Apps</h3>
         <NavButton to="/actions" label="Actions" />
         <h3>Demos</h3>
         <NavButton to="/demo/form" label="Forms" />
         <NavButton to="/demo/graph" label="Graphs" />
+        */}
       </nav>
     </header>
   );
 }
 
-// TODO: add separate pages for school and work descriptions
 function AppContainer({ me }) {
   return (
     <Switch>
