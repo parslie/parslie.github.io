3caf9fbf2223dfe7336c328dc7ed9bf26bc0507e
Viktor Holta <viktor.holta@outlook.com>
2022-05-29 21:23:34 +0200

    Clamp the size of articles

--- src/components/article.jsx
+++ src/components/article.jsx
@@ -2,9 +2,11 @@ import "../styles/article.scss";
 
 export default function Article({ children, title="" }) {
   return (
-    <article>
-      {title && <header><h1>{title}</h1></header>}
-      {children && <section>{children}</section>}
-    </article>
+    <div className="article-wrapper">
+      <article>
+        {title && <header><h1>{title}</h1></header>}
+        {children && <section>{children}</section>}
+      </article>
+    </div>
   );
 }
