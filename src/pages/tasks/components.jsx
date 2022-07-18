95cbc077d94c115e99ec55bb1995d809d66b0cd9
Viktor Holta <viktor.holta@outlook.com>
2022-07-18 12:28:08 +0200

    Create styles for mobile

--- src/pages/tasks/components.jsx
+++ src/pages/tasks/components.jsx
@@ -13,7 +13,7 @@ function TaskEntry({ me, data }) {
     <div className="task-entry">
       <header>{data.task.name}</header>
       <section>
-        <span>This task was started {startDate.toLocaleTimeString("sv-SE")} and took {secondsToText(duration)}.</span>
+        <span className="description">This task was started {startDate.toLocaleTimeString("sv-SE")} and took {secondsToText(duration)}.</span>
       </section>
     </div>
   );
@@ -42,8 +42,8 @@ function Task({ me, data }) {
       <header>{data.name}</header>
       <section>
         {data.average_duration !== 0 
-          ? <span>This task takes {secondsToText(data.average_duration)} on average.</span>
-          : <span>This task hasn't been performed yet.</span>}
+          ? <span className="description">This task takes {secondsToText(data.average_duration)} on average.</span>
+          : <span className="description">This task hasn't been performed yet.</span>}
         {me && me.is_superuser && data.is_started && <ButtonField label="End" onClick={endTask} />}
         {me && me.is_superuser && !data.is_started && <ButtonField label="Start" onClick={startTask} />}
       </section>
