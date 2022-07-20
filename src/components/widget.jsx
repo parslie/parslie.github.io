67d173ff63057c598ce0740a50679e110ed90355
Viktor Holta <viktor.holta@outlook.com>
2022-07-20 13:30:25 +0200

    Add buttons to widget header

--- src/components/widget.jsx
+++ src/components/widget.jsx
@@ -1,10 +1,11 @@
 import "./widget.scss";
 
-function Widget({ title, children }) {
+function Widget({ title, children, buttons }) {
   return (
     <div className="widget">
       <header>
         <h3 className="title">{title}</h3>
+        {buttons}
       </header>
       <section>
         {children}
