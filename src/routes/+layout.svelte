215660574d91f1b0aa70f67f3ee171ee9bcc4970
Viktor Holta <viktor.holta@outlook.com>
2025-04-15 01:55:55 +0200

    Create layout and add font 'Signika Negative'

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
+	import '$lib/styles/global.css'; // This stylesheet affects the entire site.
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header>
+	<span class="title">Viktor Holta</span>
+</header>
+
+<main>
+	{@render children()}
+</main>
+
+<style>
+	.title {
+		font-size: 2.5em;
+		font-weight: 700;
+	}
+</style>
