f2a93cbbbdd5487e439bfc644e9a178030085e1f
Viktor Holta <viktor.holta@outlook.com>
2025-04-15 02:33:49 +0200

    Style <a> links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,8 @@
 	I will put content here later. For now this page will just be filled with random stuff (i.e.
 	components and elements to use on the site in the future).
 </p>
+<p>
+	This is a regular link: <a href="https://www.youtube.com/watch?v=SerTJpflwMM">
+		Niko B - Why's this dealer?
+	</a>
+</p>
