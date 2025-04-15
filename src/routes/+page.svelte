22e4e682e3859f87a6ebf4d3e01dd0caba283222
Viktor Holta <viktor.holta@outlook.com>
2025-04-15 17:18:02 +0200

    Style code blocks and add font Fira Code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,19 @@
 		Niko B - Why's this dealer?
 	</a>
 </p>
+
+<h2>Inline Code</h2>
+<p>The <code>&lt;code&gt;</code> tag is used to display computer code.</p>
+
+<h2>Code Block</h2>
+<p>The following is a rust macro that returns the max number of a set of numbers.</p>
+<pre><code
+		>macro_rules! max &lbrace;
+    ($number:expr) =&gt; &lbrace;
+        $number
+    &rbrace;;
+    ($first:expr, $($rest:expr),+) =&gt; &lbrace;
+        std::cmp::max($first, max!($($rest),+))
+    &rbrace;;
+&rbrace;
+</code></pre>
