f8e2fba4c485497b3fc19fd9f7b2f47fd692aaca
Viktor Holta <viktor.holta@outlook.com>
2025-09-27 16:18:58 +0200

    Prepare creation of locally styled components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import Heading from "$lib/components/Heading.svelte";
+	import OldHeading from "$lib/components/old/Heading.svelte";
 	import PageHead from "$lib/components/PageHead.svelte";
 </script>
 
 <PageHead title="" />
 
-<Heading level={1} content="Welcome to my site!" />
+<OldHeading level={1} content="Welcome to my site!" />
 <p>
 	I will put content here later. For now this page will just be filled with random stuff (i.e.
 	components and elements to use on the site in the future).
--- src/routes/experiments/+page.svelte
+++ src/routes/experiments/+page.svelte
@@ -1,83 +1,19 @@
 <script lang="ts">
-	import Heading from "$lib/components/Heading.svelte";
-	import Overlay from "$lib/components/Overlay.svelte";
+	import OldHeading from "$lib/components/old/Heading.svelte";
 	import PageHead from "$lib/components/PageHead.svelte";
-
-	let overlayEnabled = $state(false);
 </script>
 
 <PageHead title="Experiments" />
 
-<Heading level={1} content="Experiments" />
+<OldHeading level={1} content="Experiments" />
 <p>This is where I experiment with designing components and elements.</p>
 
-<Heading level={2} content="Links" />
-<p>
-	This is a regular link: <a href="https://www.youtube.com/watch?v=SerTJpflwMM">
-		Niko B - Why's this dealer?
-	</a>
-</p>
+<OldHeading level={2} content="Headings" />
 
-<Heading level={2} content="Inline Code" />
-<p>The <code>&lt;code&gt;</code> tag is used to display computer code.</p>
+<OldHeading level={2} content="Links" />
 
-<Heading level={2} content="Code Block" />
-<p>The following is a rust macro that returns the max number of a set of numbers.</p>
-<pre><code
-		>macro_rules! max &lbrace;
-    ($number:expr) =&gt; &lbrace;
-        $number
-    &rbrace;;
-    ($first:expr, $($rest:expr),+) =&gt; &lbrace;
-        std::cmp::max($first, max!($($rest),+))
-    &rbrace;;
-&rbrace;
-</code></pre>
+<OldHeading level={2} content="Buttons" />
 
-<Heading level={2} content="Font Awesome 7 Free Icons" />
-<p>Font Awesome is a very useful icon pack, that also includes optional animations!</p>
-<p>
-	Firefox: <i class="fa-brands fa-firefox-browser"></i>
-	<br />
-	Check: <i class="fa-solid fa-check fa-beat-fade"></i>
-	<br />
-	Loading spinner: <i class="fa-solid fa-spinner fa-spin-pulse"></i>
-	<br />
-	PDF file: <i class="fa-regular fa-file-pdf fa-bounce"></i>
-</p>
+<OldHeading level={2} content="Code" />
 
-<Heading level={2} content="Overlays" />
-<div>
-	<Overlay enabled={overlayEnabled}>
-		<p>Cool, right?</p>
-		<p>
-			Ut efficitur hendrerit tellus at fermentum. Nullam lacinia nisi sed orci sagittis, quis varius
-			ligula pulvinar. Suspendisse non turpis erat. Vivamus tristique nisi at urna tempus porta. Sed
-			neque nisi, varius nec libero eget, viverra tincidunt neque. Vestibulum et mauris nulla. Class
-			aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam
-			consectetur, elit eu blandit ultricies, magna ante sollicitudin lectus, et consectetur ex nibh
-			non tortor. Mauris molestie nulla in sapien semper vulputate. Praesent varius erat sit amet
-			nulla commodo convallis. Maecenas dictum diam sit amet ornare pharetra. Cras at massa
-			ullamcorper, venenatis arcu eget, posuere ex. Nulla sodales tortor eu elit lacinia iaculis.
-		</p>
-	</Overlay>
-	<p>
-		Overlays are essentially elements that you can put atop other elements to fully cover them. Try
-		them out below!
-	</p>
-	<p>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet, ante at lacinia
-		vehicula, magna ipsum aliquam nisi, non vestibulum turpis dui a turpis. Vivamus dapibus leo at
-		nisi sollicitudin, viverra aliquet ipsum ullamcorper. Etiam sit amet mollis odio. Etiam vel
-		hendrerit nibh. Mauris feugiat vehicula augue. Etiam ante ante, maximus eget sem in, varius
-		pulvinar odio. Nullam mi orci, semper eu augue in, ullamcorper finibus libero. In elementum orci
-		ligula, id fringilla justo consectetur cursus.
-	</p>
-</div>
-<fieldset>
-	<legend>Overlay settings</legend>
-	<label>
-		Enabled
-		<input type="checkbox" bind:checked={overlayEnabled} />
-	</label>
-</fieldset>
+<OldHeading level={2} content="Overlay" />
